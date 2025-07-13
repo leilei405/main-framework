15848dd0f7e85f11b37a3a8cdf24272d74608242
leilei405 <1601178425@qq.com>
2025-07-13 20:02:29 +0800

    feat: ✨garfish框架 搭建微前端项目

--- lucky-site-main/src/App.tsx
+++ lucky-site-main/src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
+import { Garfish } from "garfish";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  React.useEffect(() => {
+    const garfish = new Garfish({
+      basename: "/",
+      domGetter: "#container",
+      apps: [
+        {
+          name: "react",
+          activeWhen: "/react",
+          entry: "http://localhost:3333", // 子应用入口
+        },
+      ],
+    });
+    garfish.run();
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1 className="mainTitle">
+        Vite + Garfish(微前端框架) +搭建项目 + 主应用
+      </h1>
+      <div id="container"></div>
+    </div>
   );
 }
 

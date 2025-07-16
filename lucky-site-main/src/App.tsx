1960ff2c8343b69a795c6c802688df49e536f7c8
leilei405 <1601178425@qq.com>
2025-07-16 23:51:58 +0800

    feat: ✨ 主页创建

--- lucky-site-main/src/App.tsx
+++ lucky-site-main/src/App.tsx
@@ -3,20 +3,6 @@ import { Garfish } from "garfish";
 import "./App.css";
 
 function App() {
-  React.useEffect(() => {
-    const garfish = new Garfish({
-      basename: "/",
-      domGetter: "#container",
-      apps: [
-        {
-          name: "react",
-          activeWhen: "/react",
-          entry: "http://localhost:3333", // 子应用入口
-        },
-      ],
-    });
-    garfish.run();
-  }, []);
   return (
     <div>
       <h1 className="mainTitle">
